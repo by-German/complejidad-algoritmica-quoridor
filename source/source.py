c8c4a9f21d91b27b63e54574c1fcf2bd4fa8d5a5
by-German <german280599@gmail.com>
2020-09-25 23:15:53 -0500

    generacion grafo

--- source/source.py
+++ source/source.py
@@ -1,20 +1,23 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+def grafo_tablero(n):
+    count = 0
+    G = nx.DiGraph()
+    for x in range(n):
+        for y in range(n):
+            count += 1
+            if not (y == n - 1): # enlace  horizontal
+                G.add_edge(count, count + 1)
+                G.add_edge(count + 1, count)
+            if not (x == n - 1): # enlace vertical
+                G.add_edge(count, count + n)
+                G.add_edge(count + n, count)
+            G.nodes[count]["id"] = count
+            G.nodes[count]["peso"] = 1
+    return G
+
 n = 4
-G = nx.DiGraph() # ver si es bidireccional
-# init - Generacion del tablero.
-count = 0
-for x in range(n):
-  for y in range(n):
-    count += 1
-    if not (y == n - 1): # enlace  horizontal
-      G.add_edge(count, count + 1)
-      G.add_edge(count + 1, count)
-    if not (x == n - 1): # enlace vertical
-      G.add_edge(count, count + n)
-      G.add_edge(count + n, count)
-    G.nodes[count]["id"] = count
-    G.nodes[count]["peso"] = 1
+G = grafo_tablero(n)
 
 nx.draw(G, with_labels = True)
